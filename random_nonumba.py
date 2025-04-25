db272f3d25988f3683341b6a53e972022e63dd79
Timtta <adef17286@gmail.com>
2025-04-25 16:31:10 +0200

    random_nonumba.py

--- random_nonumba.py
+++ random_nonumba.py
@@ -1,9 +1,20 @@
 import os
 
-def generate_random_bytes(filename, size):
-    with open(filename, 'wb') as f:
+def generate_random_bytes(output_filename, size):
+    with open(output_filename, 'wb') as f:
         f.write(os.urandom(size))
 
-filename = 'random_bytes.bin'
-size = 10
-generate_random_bytes(filename, size)
+def get_file_size(filename):
+    try:
+        return os.path.getsize(filename)
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+        return None
+
+input_filename = 'input_bytes.bin'
+output_filename = 'random_bytes.bin'
+
+size = get_file_size(input_filename)
+if size is not None:
+    generate_random_bytes(output_filename, size)
+    print(f"Generated {size} random bytes and wrote them to '{output_filename}'.")
